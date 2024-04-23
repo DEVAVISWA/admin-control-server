5ae5920175e57ee192b3e1fff231e15a49f3a0ed
DEVAVISWA <devaviswa15@gmail.com>
2024-04-23 19:58:55 +0530

    cors modified

--- server.js
+++ server.js
@@ -1,39 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const clientRoutes = require('./routes/client.routes');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const clientRoutes = require("./routes/client.routes");
+const cors = require("cors");
 
-
-require('dotenv').config();
+require("dotenv").config();
 // Create express app
-const app = express()
+const app = express();
 
 app.use(express.json());
-app.use(cors())
+
+//cors
+const corsOptions = {
+  origin: [
+    "https://admin-control-client.vercel.app/",
+    "http://127.0.0.1:3002",
+  ],
+};
+app.use(cors(corsOptions));
+// app.use(cors());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next();
+  console.log(req.path, req.method);
+  next();
 });
 // Set up a route
-app.use('/client', clientRoutes);
+app.use("/client", clientRoutes);
 
 //setup a default route
-app.use('/',(req,res)=>{
-    res.send('<h1>Admin control by Deva Viswa G</h1>')
-})
+app.use("/", (req, res) => {
+  res.send("<h1>Admin control by Deva Viswa G</h1>");
+});
 
 // Set up the server
 
 // connect to the database
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('Connected to the database')
-        // Start the server
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on http://localhost:${process.env.PORT}`)
-        })
-    })
-    .catch((err) => {
-        console.log('Error connecting to the database', err)
-    })
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to the database");
+    // Start the server
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to the database", err);
+  });
